a33e3b371814b6e0c459923195da3dc9a0364db4
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-24 14:11:03 +0000

    can log in and out from navbar links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # makes them helper methods and thus available to views
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # ||= is memoization - will only perform if @current_user doesn't exist, to stop it querying the database repeatedly
+    @current_user ||= Student.find(session[:student_id]) if session[:student_id]
+  end
+
+  def logged_in?
+    # !! makes it a boolean return - ie. if current_user is there, true, if not, false
+    !!current_user
+  end
 end
