7d28b7cc3386767089ba2d27f1f7e5392303f611
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-25 11:57:03 +0000

    add edit & delete icons to course card

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,31 @@
 class CoursesController < ApplicationController
-  skip_before_action :require_user
-  
+
   def index 
     @courses = Course.all
   end
 
   def new
+    @course = Course.new
+  end
 
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      flash[:notice] = "Course successfully created"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :short_name, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'courses#index'
-  get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
   resources :students, except: [:destroy]
+  resources :courses
   get 'login', to: 'logins#new'
   post 'login', to: 'logins#create'
   delete 'logout', to: 'logins#destroy'
