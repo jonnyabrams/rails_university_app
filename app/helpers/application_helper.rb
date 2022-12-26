b9e2740650e40d59de7997176e2928afc59b231a
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-26 11:41:07 +0000

    link to view profile in navbar dropdown (only if signed in)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
   def session_link
    if (logged_in?)
-    link_to("logout", logout_path, method: :delete)
+    link_to("Log Out", logout_path, method: :delete)
    else
-    link_to("login", login_path)
+    link_to("Log In", login_path)
    end
   end
 end
